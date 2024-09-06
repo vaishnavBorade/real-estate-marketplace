e8cf9ae7f3436030d2fc9ce548b1982517ba9e92
vaishnavBorade <115063068+vaishnavBorade@users.noreply.github.com>
2024-09-06 12:04:22 +0530

    added header component

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,12 @@ import SignUp from './pages/SignUp'
 import About from './pages/About'
 import Profile from './pages/Profile'
 import Home from './pages/Home'
+import Header from './components/Header'
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
