5b7c579da6dff7e9443984a617db7e7a1fee25e5
vaishnavBorade <115063068+vaishnavBorade@users.noreply.github.com>
2024-09-06 13:36:21 +0530

    created routes and controllers

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,3 @@
+export const test = (req,res)=>{
+  res.send('api route is working');
+}
--- api/index.js
+++ api/index.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from'dotenv'
+import userRouter from './routes/user.route.js'
+
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(()=>{
+mongoose.connect(process.env.MONGO)
+.then(()=>{
     console.log("Connected to MongoDB");
-  }
-).catch((err)=>{
+})
+.catch((err)=>{
   console.log(err);
 });
 
@@ -16,7 +19,4 @@ app.listen(3000, () => {
   console.log('Server is running on PORT 3000');
 });
 
-app.get('/',(req,res)=>{
-    res.send('Hello World!');
-  }
-);
+app.use('/api/user', userRouter);
