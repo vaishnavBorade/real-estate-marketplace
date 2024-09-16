668f220814417c27ea9fd5c715415759e5b11f7b
vaishnavBorade <115063068+vaishnavBorade@users.noreply.github.com>
2024-09-17 01:36:35 +0530

    complete

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,25 +1,28 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatar:{
+      type: String,
+      default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password:{
-    type: String,
-    required: true,
-  },
-  avatar: {
-    type: String,
-    default: "https://static-00.iconduck.com/assets.00/user-icon-1024x1024-dtzturco.png",
-  },
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 const User = mongoose.model('User', userSchema);
 
